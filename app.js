cc01fa2f539ddb40fe4bc6e2f7b535a796d76540
Chinmay Shringi <chinmay@logicwind.co>
2022-02-24 10:21:31 +0530

    validator js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express=require('express');
-const app=express();
-var bodyParser=require('body-parser');
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
 app.use(require('./utils/cors'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use('/',require('./routes'));
+app.use('/', require('./routes'));
 
-
-
-app.listen(process.env.PORT || 5555,()=>{
-    console.log("Server Started at port 5555 ");
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server Started at port 5000 ');
 });
--- database.js
+++ database.js
@@ -1,52 +1,49 @@
-const admin=require('firebase-admin');
+const admin = require('firebase-admin');
 
 var serviceAccount = require('./admin.json');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://itrfirebase.firebaseio.com",
-    storageBucket: "gs://itrfirebase.appspot.com",
-    authDomain: "itrfirebase.firebaseapp.com",
-  });
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    'https://fir-validation-a6570-default-rtdb.asia-southeast1.firebasedatabase.app/',
+});
 
-var db=admin.database();
-var userRef=db.ref("users");
+var db = admin.database();
+var userRef = db.ref('users');
 
-const userOperation={
-    addUser(obj,res){
-        var oneUser=userRef.child(obj.roll);
-        oneUser.update(obj,(err)=>{
-            if(err){
-                res.status(300).json({"msg":"Something went wrong","error":err});
-            }
-            else{
-                res.status(200).json({"msg":"user created sucessfully"});
-            }
-        })
-    },
-    demoUser(obj,res){
-        var userRefdemo=db.ref("demousers");
-        var oneUser=userRefdemo.child(obj.roll);
-        oneUser.push(obj,(err)=>{
-            if(err){
-                res.status(300).json({"msg":"Something went wrong","error":err});
-            }
-            else{
-                res.status(200).json({"msg":"user created sucessfully"});
-            }
-        })
-    },
-    getUsers(res){
-        userRef.once('value',function(snap){
-            res.status(200).json({"users":snap.val()});
-        })
-    },
-    getOneUser(obj,res){
-        var userRefdemo=db.ref("users");
-        var oneUser=userRefdemo.child(obj.roll);
-        oneUser.once('value',function(snap){
-            res.status(200).json({"user":snap.val()});
-        })
-    }
-}
+const userOperation = {
+  addUser(obj, res) {
+    var oneUser = userRef.child(obj.roll);
+    oneUser.update(obj, (err) => {
+      if (err) {
+        res.status(300).json({ msg: 'Something went wrong', error: err });
+      } else {
+        res.status(200).json({ msg: 'user created sucessfully' });
+      }
+    });
+  },
+  demoUser(obj, res) {
+    var userRefdemo = db.ref('demousers');
+    var oneUser = userRefdemo.child(obj.roll);
+    oneUser.push(obj, (err) => {
+      if (err) {
+        res.status(300).json({ msg: 'Something went wrong', error: err });
+      } else {
+        res.status(200).json({ msg: 'user created sucessfully' });
+      }
+    });
+  },
+  getUsers(res) {
+    userRef.once('value', function (snap) {
+      res.status(200).json({ users: snap.val() });
+    });
+  },
+  getOneUser(obj, res) {
+    var userRefdemo = db.ref('users');
+    var oneUser = userRefdemo.child(obj.roll);
+    oneUser.once('value', function (snap) {
+      res.status(200).json({ user: snap.val() });
+    });
+  },
+};
 
-module.exports=userOperation;
+module.exports = userOperation;
